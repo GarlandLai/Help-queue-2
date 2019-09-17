6afac33d1a9efcc6f1368b0927ab8cdfdf5c3bb3
Garland Lai <garlandlai24@gmail.com>
2019-09-17 16:33:35 -0700

    connected selectedticket to Redux. Left off at 'retrieving redux state'

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'moment';
+import { connect } from 'react-redux';
 
 function Ticket(props){
   var myStyle = {
     backgroundColor: '#ecf0f1',
     fontFamily: 'monospace',
-    fontSize: '24px'
+    fontSize: '24px',
   };
   var myOtherStyle = {
-    fontSize: '15px'
+    fontSize: '15px',
   };
+  function handleSavingSelectedTicket(ticketId) {
+    const { dispatch } = props;
+    const action = {
+      type: 'SELECT_TICKET',
+      ticketId: ticketId,
+    };
+    dispatch(action);
+  }
 
   const ticketInformation =
-    <div>
-      <h3>{props.location} - {props.names}</h3>
-      <h4>{props.formattedWaitTime} ago</h4>
-      <hr/>
-    </div>;
-  if (props.currentRouterPath === '/admin'){
+  <div>
+    <h3>{props.location} - {props.names}</h3>
+    <h4>{props.formattedWaitTime} ago</h4>
+    <hr/>
+  </div>;
+  if (props.currentRouterPath === '/admin') {
     return (
-      <div onClick={() => {props.onTicketSelection(props.ticketId);}}>
+      <div onClick={() => {handleSavingSelectedTicket(props.ticketId);}}>
+
         {ticketInformation}
       </div>
     );
@@ -43,4 +53,4 @@ Ticket.propTypes = {
   ticketId: PropTypes.string.isRequired
 };
 
-export default Ticket;
+export default connect()(Ticket);
